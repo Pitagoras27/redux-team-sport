90ce845b7e7992cec2e6c4e068f641709c44c9c2
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-17 16:04:11 -0500

    Se agrega mockup de initialState

--- src/initialState.js
+++ src/initialState.js
@@ -1,12 +1,67 @@
 export const initialState = {
   jugadores: [{
     id: 1,
-    nombre: "Juan Carlitos",
-    foto: "https://api.ed.team/files/avatars/001803a2-cf00-4a26-8bac-c77c15dcb1d2.jpg"
+    name: "charmeleon",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png"
   },
   {
     id: 2,
-    nombre: "Beto Quiroga",
-    foto: "https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg",
+    name: "venusaur",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/3.png",
+  },
+  {
+    id: 3,
+    name: "ivysaur",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png",
+  },
+  {
+    id: 4,
+    name: "caterpie",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/10.png",
+  },
+  {
+    id: 5,
+    name: "bulbasaur",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+  },
+  {
+    id: 6,
+    name: "charizard",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png",
+  },
+  {
+    id: 7,
+    name: "squirtle",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png",
+  },
+  {
+    id: 8,
+    name: "metapod",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/11.png",
+  },
+  {
+    id: 9,
+    name: "wartortle",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    id: 10,
+    name: "butterfree",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/12.png",
+  },
+  {
+    id: 11,
+    name: "blastoise",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/9.png",
+  },
+  {
+    id: 12,
+    name: "weedle",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/13.png",
+  },
+  {
+    id: 13,
+    name: "kakuna",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/14.png",
   }]
 }
